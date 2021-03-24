772051b75ebc1b245efb2f18ca9de68300ece04a
Maxim Spesivykh <spamol@ya.ru>
2021-03-24 17:00:38 +0300

    сервер наносит первый удар

--- src/service/cli/constants.js
+++ src/service/cli/constants.js
@@ -6,6 +6,7 @@ const ANNOUNCE_LENGTH = 5;
 const MONTH_RESTRICT = 3;
 const FILE_NAME = 'mocks.json';
 const TEXT_FILE_PATH = 'data';
+const DEFAULT_PORT = 3000;
 
 module.exports = {
   DEFAULT_COUNT,
@@ -13,5 +14,6 @@ module.exports = {
   ANNOUNCE_LENGTH,
   MONTH_RESTRICT,
   FILE_NAME,
-  TEXT_FILE_PATH
+  TEXT_FILE_PATH,
+  DEFAULT_PORT
 };
